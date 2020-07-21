8aec3f191dc9600ba56657f700bf58d1f429a074
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-07-21 15:13:09 -0700

    sorting completed

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,16 +1,54 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
+    # starting at the beginning of `a` and `b`
+    # compare the next value of each
+    # add smallest to `merged_arr`
+
     num_elements = len(arrA) + len(arrB)
     merged_arr = []
 
-    # Your code here
+    for n in arrA and arrB:
+        if arrA[n] > arrB[n]:
+            merged_arr.append(arrB[n])
+            merged_arr.append(arrA[n])
+
+        elif arrB[n] > arrA[n]:
+            merged_arr.append(arrA[n])
+            merged_arr.append(arrB[n])
 
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
+    # recursively call merge_sort() on LHS
+    # recursively call merge_sort() on RHS
+    # merge sorted pieces
+    if len(arr)>1: 
+        m = len(arr)//2
+        left = arr[:m] 
+        right = arr[m:] 
+        left = merge_sort(left) 
+        right = merge_sort(right) 
+  
+        arr =[] 
+  
+        while len(left)>0 and len(right)>0: 
+            if left[0]<right[0]: 
+                arr.append(left[0]) 
+                left.pop(0) 
+            else: 
+                arr.append(right[0]) 
+                right.pop(0) 
+
+
+    #append 
+        for i in left: 
+            arr.append(i) 
+        for i in right: 
+            arr.append(i) 
+                  
+    return arr 
 
 
     return arr
@@ -21,8 +59,8 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
-
+    pass
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
